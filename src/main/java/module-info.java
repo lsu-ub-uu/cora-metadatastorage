37f00fe8ced0897e531914d0eaf90df1e3fd8070
pere <pere@243ae0b228c0>
2022-10-17 11:49:19 +0000

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module se.uu.ub.cora.metadatastorage {
+	// requires se.uu.ub.cora.logger;
+	// requires transitive se.uu.ub.cora.storage;
+	// requires transitive se.uu.ub.cora.spider;
+	requires transitive se.uu.ub.cora.bookkeeper;
+
+	// exports se.uu.ub.cora.metadatastorage;
+
+	// provides MetadataStorageViewInstanceProvider with MetadataStorageViewInstanceProviderImp;
+}
